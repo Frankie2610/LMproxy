2e88fa7d331da980a5dd748836af52a3b89a1a85
Văn Gia Quyền <giaquyenvan@gmail.com>
2025-02-04 19:23:04 +0700

    okelathelast

--- api/LMserver.js
+++ api/LMserver.js
@@ -60,7 +60,7 @@ app.post('/api/LMserver.js', async (req, res) => {
                     namespace: "custom",
                     key: "product_views",
                     type: "list.number_integer",
-                    value: "[${productViews}]"
+                    value: "[${productViews}/2]"
                 }
             ]) {
                 metafields {
